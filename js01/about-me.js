c893ba7754de3d26dd6f8b1fb2e9f8058125ca76
ConnorHarrisonAkaKobe <mc_charrison27@mcjvs2.org>
2025-11-03 09:16:14 -0500

    your message

--- js01/about-me.js
+++ js01/about-me.js
@@ -2,11 +2,12 @@
 // A Pop Up that says Hello when you open
 alert("Hello!")
 
+console.log ("Hello! Hru!")
+console.log ("Hopefully Good!")
+console.log ("Because you matter!")
+
     // a heading for the website
     document.write('<h1>Connor Harrison</h1>')
 
     // the three paragraphs
     document.write('<p>I like basketball, making a mod on Gdot as a cool way to practice video games, and I have 6 siblings. I was born in Rhode Island of New England, when I was a freshman, I moved to Ohio as it is much safer and better to live in. My favorite food to eat, cook, and enjoy is American Chop Suey!</p>')
-
-
-
